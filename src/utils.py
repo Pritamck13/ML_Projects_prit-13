fccd6942b2a8c901f1429fc288c87a8f85a73b1f
pritam.changkakoty@hotmail.com <pritam.changkakoty@hotmail.com>
2024-08-29 12:14:34 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ from sklearn.metrics import r2_score
 import pickle
 
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def saved_object(file_path,obj):
     try:
@@ -19,18 +20,31 @@ def saved_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models): # try separately on ipynb. 
+def evaluate_model(X_train,y_train,X_test,y_test,models,param): # try separately on ipynb. 
     try:
         report = {}
-        for name,model in models.items():
-            fitted = model.fit(X_train,y_train)
-            y_train_pred = fitted.predict(X_train)
-            y_test_pred = fitted.predict(X_test)
-            train_model_score = r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
-            report[name] = test_model_score
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+            
 
         # for i in range(len(list(models))):
         #     model = list(models.values())[i]
